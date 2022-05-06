6e615c026079b4097c8b057340511d092ac9e186
Илья Матвеев <matweev.ilja@yandex.ru>
2022-05-07 00:56:28 +0300

    Подправил

--- src/js/modules/aboutText.js
+++ src/js/modules/aboutText.js
@@ -1,20 +1,20 @@
-const aboutText = document.getElementById("aboutText");
-const aboutShadow = document.getElementById("aboutShadow");
+// const aboutText = document.getElementById("aboutText");
+// const aboutShadow = document.getElementById("aboutShadow");
 
-function offset(el) {
-  var rect = el.getBoundingClientRect(),
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  return { top: rect.top + scrollTop };
-}
+// function offset(el) {
+//   var rect = el.getBoundingClientRect(),
+//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   return { top: rect.top + scrollTop };
+// }
 
-function aboutTextOpen() {
-  const abouTextOffset = offset(aboutText);
-  let scrollY = window.scrollY;
+// function aboutTextOpen() {
+//   const abouTextOffset = offset(aboutText);
+//   let scrollY = window.scrollY;
 
-  if (abouTextOffset.top - 141 < scrollY) {
-    aboutShadow.setAttribute("style", "height: 0");
-    aboutText.setAttribute("style", "max-height: 1000vh");
-  }
-}
+//   if (abouTextOffset.top - 141 < scrollY) {
+//     aboutShadow.setAttribute("style", "height: 0");
+//     aboutText.setAttribute("style", "max-height: 1000vh");
+//   }
+// }
 
-window.addEventListener("scroll", aboutTextOpen);
+// window.addEventListener("scroll", aboutTextOpen);
